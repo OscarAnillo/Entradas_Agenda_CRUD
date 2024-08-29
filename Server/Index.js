3a7a11e5f984d5f531eb8c82ba2d54bdf16cd318
Oscar Anillo <oscarani14@gmail.com>
2024-08-29 17:20:57 -0500

    move db connection from model to index.js

--- Server/Index.js
+++ Server/Index.js
@@ -5,18 +5,29 @@ const morgan = require("morgan");
 const config = require("./Utils/config");
 const PersonRouter = require("./Controllers/contacts");
 const middleware = require("./Utils/middleware");
+const mongoose = require("mongoose");
 const app = express();
 
+/* Database connection */
+mongoose
+  .connect(config.MONGO_URI)
+  .then(() => {
+    console.log("Connecting to the database...");
+    console.log("Connected!");
+  })
+  .catch((err) => console.log(err));
+
 /* Middleware */
-//app.use(express.static(path.join(__dirname, "Client/contacts-entry-fe/dist")));
 app.use(express.static("Client/contacts-entry-fe/dist"));
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(morgan("common"));
 
 app.use("/api/persons", PersonRouter);
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
-const port = config.PORT || 3005;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.listen(config.PORT, () =>
+  console.log(`Server listening on port ${config.PORT}`)
+);
--- Server/Model/Person.js
+++ Server/Model/Person.js
@@ -1,13 +1,4 @@
 const mongoose = require("mongoose");
-const config = require("../Utils/config");
-
-mongoose
-  .connect(config.MONGO_URI)
-  .then(() => {
-    console.log("Connecting to the database...");
-    console.log("Connected!");
-  })
-  .catch((err) => console.log(err));
 
 const personSchema = new mongoose.Schema({
   name: {
